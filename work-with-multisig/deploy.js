bc2253a7a20d5ac30e22448e039633cbf41d1755
Eugenia M <44024692+joydark@users.noreply.github.com>
2020-07-10 11:44:45 +0300

    Work with multisig (#1)
    * Working with multisig contract

---
+++ work-with-multisig/deploy.js
@@ -0,0 +1,88 @@
+const { TONClient } = require('ton-client-node-js');
+const fs = require('fs');
+const path = require('path');
+const keyPairFile = path.join(__dirname, 'keyPair.json');
+
+const multisigContractPackage = {
+    abi: require('../ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json'),
+    imageBase64: fs.readFileSync('../ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.tvc').toString('base64'),
+};
+
+// Account is active when contract is deployed.
+const ACCOUNT_TYPE_ACTIVE = 1;
+
+// Account is uninitialized when contract is not deployed yet.
+const ACCOUNT_TYPE_UNINITIALIZED = 0;
+
+// Number of tokens required to deploy the contract.
+// See https://docs.ton.dev/86757ecb2/p/6207cd-estimate-fees on how to calculate definite number.
+const CONTRACT_REQUIRED_DEPLOY_TOKENS = 500_000_000;
+
+
+(async () => {
+    try {
+        //See https://docs.ton.dev/86757ecb2/p/069155-ton-os-se/b/09fbbd
+        const tonClient = await TONClient.create({
+            servers: ['net.ton.dev'],
+            //See https://docs.ton.dev/86757ecb2/p/88321a-reliable-message-delivery
+            messageExpirationTimeout: 30000, //timeout value in ms after the message count as expired
+            retriesCount: 3,  //the number of retries attempts
+        });
+
+        if (!fs.existsSync(keyPairFile)) {
+            console.log('Please use preparation.js to generate key pair and seed phrase');
+            process.exit(1);
+        }
+
+        const keyPair = JSON.parse(fs.readFileSync(keyPairFile, 'utf8'));
+
+        const futureAddress = (await tonClient.contracts.createDeployMessage({
+            package: multisigContractPackage,
+            constructorParams: {
+                owners: [`0x${keyPair.public}`],//Multisig owner public key
+                reqConfirms: 0,  //Multisig required confirms
+            },
+            keyPair: keyPair,
+        })).address;
+
+        //query account type, balance and code to analyse if it is possible to deploy the contract
+        const accountData = await tonClient.queries.accounts.query({
+                // See https://docs.ton.dev/86757ecb2/p/772196-collection-query-methods for syntax
+                id: { eq: futureAddress }
+            },
+            'acc_type balance code');
+
+        if (accountData.length === 0) {
+            console.log(`You need to transfer at least 0.5 tokens for deploy to ${futureAddress}.`);
+            process.exit(1);
+        }
+
+        if (accountData[0].acc_type == ACCOUNT_TYPE_ACTIVE) {
+            console.log('Contract is already deployed');
+            process.exit(1);
+        }
+
+        // Balance is stored as HEX so we need to convert it.
+        if (accountData[0].acc_type == ACCOUNT_TYPE_UNINITIALIZED && BigInt(accountData[0].balance) <= BigInt(CONTRACT_REQUIRED_DEPLOY_TOKENS)) {
+            console.log('Balance is too low for deploy');
+            process.exit(1);
+        }
+
+       //Deploy multisig contract with 1 custodian having keyPair.public key and requiring 0 confirmations.
+        await tonClient.contracts.deploy({
+            package: multisigContractPackage,
+            constructorParams: {
+                owners: [`0x${keyPair.public}`],
+                reqConfirms: 0,
+            },
+            keyPair: keyPair,
+        });
+
+        console.log(`Contract is successfully deployed. You can play with your multisig wallet now at ${futureAddress}`);
+
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+})();
---
+++ work-with-multisig/work-with-multisig.js
@@ -0,0 +1,178 @@
+const { TONClient } = require('ton-client-node-js');
+const fs = require('fs');
+const path = require('path');
+const keyPairFile = path.join(__dirname, 'keyPair.json');
+
+// Address to send tokens to
+const destinationAddress = '0:2bb4a0e8391e7ea8877f4825064924bd41ce110fce97e939d3323999e1efbb13';
+
+const multisigContractPackage = {
+    abi: require('../ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.abi.json'),
+    imageBase64: fs.readFileSync('../ton-labs-contracts/solidity/safemultisig/SafeMultisigWallet.tvc').toString('base64'),
+};
+
+const ACCOUNT_TYPE_UNINITIALIZED = 0;
+
+
+(async () => {
+    try {
+        //You can also connect to NodeSE https://docs.ton.dev/86757ecb2/p/069155-ton-os-se/b/09fbbd
+        //Read more about message expiration and retries here https://docs.ton.dev/86757ecb2/p/88321a-reliable-message-delivery
+        const tonClient = await TONClient.create({
+            servers: ['net.ton.dev'],
+            messageExpirationTimeout: 30000,
+            retriesCount: 3,
+        });
+
+        if (!fs.existsSync(keyPairFile)) {
+            console.log('Please use preparation.js to generate key pair and seed phrase');
+            process.exit(1);
+        }
+
+        const keyPair = JSON.parse(fs.readFileSync(keyPairFile, 'utf8'));
+
+        const address = (await tonClient.contracts.createDeployMessage({
+            package: multisigContractPackage,
+            constructorParams: {
+                owners: [`0x${keyPair.public}`],//Multisig owner public key
+                reqConfirms: 0,  //Multisig required confirms
+            },
+            keyPair: keyPair,
+        })).address;
+
+        const accountData = await tonClient.queries.accounts.query({
+                id: { eq: address }
+            },
+            'acc_type balance code');
+
+        if (accountData.length === 0) {
+            console.log(`You need to transfer at least 0.5 tokens and use deploy.js to deploy the contract.`);
+            process.exit(1);
+        }
+
+        // See https://docs.ton.dev/86757ecb2/p/598b5c-runlocal-method
+        const response = await tonClient.contracts.runLocal({
+            address: address,
+            abi: multisigContractPackage.abi,
+            functionName: 'getCustodians',
+            input: {},
+            keyPair: keyPair
+        });
+
+        console.log('Сustodians list:', response.output.custodians);
+
+        const transaction = {
+            dest: destinationAddress,
+            value: 100_000_000,
+            bounce: false,
+            allBalance: false,
+            payload: ''
+        };
+
+        const sentTransactionInfo = await tonClient.contracts.run({
+            address: address,
+            abi: multisigContractPackage.abi,
+            functionName: 'submitTransaction',
+            input: transaction,
+            keyPair: keyPair
+        });
+
+        // See https://docs.ton.dev/86757ecb2/p/35a3f3-field-descriptions
+        console.log("Transaction info:")
+
+        console.log("Id:")
+        console.log(sentTransactionInfo.transaction.id);
+
+        console.log("Block id:")
+        console.log(sentTransactionInfo.transaction.block_id);
+
+        console.log("Account address:")
+        console.log(destinationAddress);
+
+        console.log("Logical time:")
+        console.log(sentTransactionInfo.transaction.lt);
+
+        console.log("Transaction inbound message ID:")
+        console.log(sentTransactionInfo.transaction.in_msg);
+
+        console.log("Transaction outbound message IDs:")
+        console.log(sentTransactionInfo.transaction.out_msgs);
+
+        //Print the list of all transactions of this account
+        //Read more about aggregation methods here: https://docs.ton.dev/86757ecb2/p/772196-collection-query-methods/t/24ab01
+        const transactionsCount = (await tonClient.queries.transactions.aggregate({
+            filter: {
+                account_addr: { eq: address }
+            },
+            fields: [{
+                field: 'id',
+                fn: 'COUNT',
+            }],
+        }))[0];
+        console.log(`All ${transactionsCount} account transactions id:`);
+
+
+        let logicalTime = "0x0";
+
+        for (let i = 0; i < (transactionsCount / 10); i++) {
+            let result = await tonClient.queries.transactions.query({
+                filter: {
+                    account_addr: { eq: address },
+                    lt: { gt: logicalTime }
+                },
+                orderBy: [{
+                    path: 'now',
+                    direction: 'ASC',
+                }, {
+                    path: 'lt',
+                    direction: 'ASC',
+                }
+                ],
+                limit: 10,
+                result: 'id now lt',
+            });
+
+            for (let y = 0; y < result.length; y++) {
+                console.log("id " + result[y].id + " transaction time(now) " + result[y].now);
+                logicalTime = result[y].lt;
+            }
+        }
+        // Convert address to different types
+        console.log("Multisig address in HEX:")
+        let convertedAddress = (await tonClient.contracts.convertAddress({
+            address: address,
+            convertTo: 'Hex',
+        })).address;
+        console.log(convertedAddress);
+
+        console.log("Multisig non-bounce address in Base64:")
+        convertedAddress = (await tonClient.contracts.convertAddress({
+            address: address,
+            convertTo: 'Base64',
+            base64Params: {
+                test: false,
+                bounce: false,
+                url: false,
+            },
+        })).address;
+        console.log(convertedAddress);
+
+        console.log("Multisig bounce address in Base64:")
+        convertedAddress = (await tonClient.contracts.convertAddress({
+            address: address,
+            convertTo: 'Base64',
+            base64Params: {
+                test: false,
+                bounce: true,
+                url: false,
+            },
+        })).address;
+        console.log(convertedAddress);
+
+
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+})();
