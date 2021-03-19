f95181a94e13694a06cf3733b9bbca08ac8612f7
Michael Vlasov <melsomino@gmail.com>
2021-03-19 18:42:14 +0500

    NEW: port to `@tonclient/appkit`

--- examples/multisig/error_produce.js
+++ examples/multisig/error_produce.js
@@ -1,13 +1,12 @@
 // this example produces error intentionally
-const {libNode} = require("@tonclient/lib-node");
+const { libNode } = require("@tonclient/lib-node");
 const fs = require("fs");
 const path = require("path");
 const {
-    Account,
     signerKeys,
     TonClient,
 } = require("@tonclient/core");
-const {MultisigContract} = require("./contracts");
+const { MultisigContract } = require("./contracts");
 
 const keyPairFile = path.join(__dirname, "keyPair.json");
 const keyPairFile2 = path.join(__dirname, "keyPair2.json");
@@ -19,19 +18,18 @@ const ACCOUNT_TYPE_ACTIVE = 1;
 const recipient = "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a6312490415";
 
 (async () => {
+    TonClient.useBinaryLibrary(libNode);
+    const client = new TonClient({
+        network: {
+            // server_address:'net.ton.dev',
+            endpoints: ["net.ton.dev"],
+            message_retries_count: 0, // default = 5
+        },
+        abi: {
+            message_expiration_timeout: 30000, // default = 40000
+        },
+    });
     try {
-        TonClient.useBinaryLibrary(libNode);
-        TonClient.defaultConfig = {
-            network: {
-                // server_address:'net.ton.dev',
-                endpoints: ["net.ton.dev"],
-                message_retries_count: 0, // default = 5
-            },
-            abi: {
-                message_expiration_timeout: 30000, // default = 40000
-            },
-        };
-
         if (!fs.existsSync(keyPairFile)) {
             console.log("Please use preparation.js to generate key pair and seed phrase");
             process.exit(1);
@@ -39,7 +37,7 @@ const recipient = "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a63124
 
         const keyPair = JSON.parse(fs.readFileSync(keyPairFile, "utf8"));
 
-        const acc = new Account(MultisigContract, {signer: signerKeys(keyPair)});
+        const acc = new Account(MultisigContract, { signer: signerKeys(keyPair) });
         const address = await acc.getAddress();
         console.log(address);
         let info;
@@ -98,7 +96,7 @@ const recipient = "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a63124
 
         // Convert address to different types
         console.log("Multisig address in HEX:");
-        let convertedAddress = (await TonClient.default.utils.convert_address({
+        let convertedAddress = (await client.utils.convert_address({
             address,
             output_format: {
                 type: "Hex",
@@ -107,7 +105,7 @@ const recipient = "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a63124
         console.log(convertedAddress);
 
         console.log("Multisig non-bounce address in Base64:");
-        convertedAddress = (await TonClient.default.utils.convert_address({
+        convertedAddress = (await client.utils.convert_address({
             address,
             output_format: {
                 type: "Base64",
@@ -119,7 +117,7 @@ const recipient = "0:ece57bcc6c530283becbbd8a3b24d3c5987cdddc3c8b7b33be6e4a63124
         console.log(convertedAddress);
 
         console.log("Multisig bounce address in Base64:");
-        convertedAddress = (await TonClient.default.utils.convert_address({
+        convertedAddress = (await client.utils.convert_address({
             address,
             output_format: {
                 type: "Base64",
