0d28129c96786d17abf98249ef0d8263206b72e2
Eugenia Makarova <makarova.evgenia@gmail.com>
2021-04-09 19:01:11 +0300

    Add high level stage example

---
+++ examples/fee-calculation/HelloContract.js
@@ -0,0 +1,33 @@
+module.exports = {
+    HelloContract: {
+        abi: {
+            "ABI version": 2,
+            "header": ["time", "expire"],
+            "functions": [
+                {
+                    "name": "constructor",
+                    "inputs": [],
+                    "outputs": [],
+                },
+                {
+                    "name": "touch",
+                    "inputs": [],
+                    "outputs": [],
+                },
+                {
+                    "name": "getTimestamp",
+                    "inputs": [],
+                    "outputs": [
+                        {
+                            "name": "value0",
+                            "type": "uint256",
+                        },
+                    ],
+                },
+            ],
+            "data": [],
+            "events": [],
+        },
+        tvc: "te6ccgECGgEAAfoAAgE0AwEBAcACAEPQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAgAib/APSkICLAAZL0oOGK7VNsEvShBgQBCvSkIPShBQAAAgEgCQcBBv9/IQgBwNs80wABn4ECANcYIPkBWPhC+RDyqN7TPwGOHfhDIbkgnzAg+COBA+iogggbd0Cgud6TIPhj4PI02DDTHwH4I7zyudMfIcEDIoIQ/////byxk1vyPOAB8AH4R26TMPI83gwCASANCgEPvUWq+f/CC3QLAiDjAPhG8nNx+GbR+AD4I/hqDBAAeO1E0CDXScIBjhTT/9M/0wDXC//4an/4Yfhm+GP4Yo4b9AVw+GpwAYBA9A7yvdcL//hicPhjcPhmf/hh4gIBIBEOAQ+7cxLkX4QW6A8CEuMA0fgA+CP4ahgQAQrbPH/4ZxYCASAZEgEPufOskH8ILdATAgbjANEYFAJY2zwhwP+OIyPQ0wH6QDAxyM+HIM6AYM9Az4HPgc+SfnWSDiHPC//JcPsA3jAXFQEK4wB/+GcWAC74QsjL//hDzws/+EbPCwD4SgHL/8ntVAAE+EoALu1E0NP/0z/TANcL//hqf/hh+Gb4Y/hiAGDccCLQ1wsDqTgA3CHHANwh0x8h3SHBAyKCEP////28sZNb8jzgAfAB+EdukzDyPN4=",
+    }
+};
