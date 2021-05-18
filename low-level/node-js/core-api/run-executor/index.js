bcfb90befd9ec35f2818677873c020b43b4398d1
elasticLove1 <elasticLove@protonmail.ch>
2021-05-18 23:45:09 +0300

    run_executor sample improved: capabilities for contract debug and fees estimation

--- low-level/node-js/core-api/run-executor/index.js
+++ low-level/node-js/core-api/run-executor/index.js
@@ -1,25 +1,59 @@
 const { libNode } = require("@tonclient/lib-node");
 // https://github.com/tonlabs/ton-labs-contracts/tree/master/solidity/safemultisig
 const { SafeMultisigWalletContract } = require("./SafeMultisigWalletContract.js");
-const {
-    signerKeys,
-    TonClient,
-} = require("@tonclient/core");
+const { signerKeys, abiContract, TonClient } = require("@tonclient/core");
+
 TonClient.useBinaryLibrary(libNode);
+
+const fs = require('fs');
+const path = require('path');
+const giverKeyPairFileName = 'GiverV2.keys.json';
+const giverKeyPairFile = path.join(__dirname, giverKeyPairFileName);
+// ABI and imageBase64 of a binary Hello contract
+const {GiverV2} = require('./GiverV2.js');
+const { Console } = require("console");
+// Address of giver on TON OS SE
+const giverAddress = '0:b5e9240fc2d2f1ff8cbb1d1dee7fb7cae155e5f6320e585fcc685698994a19a5';
+
+// Function that requests 10 local test tokens from TON OS SE giver
+async function get_tokens_from_giver(client, account) {
+    if (!fs.existsSync(giverKeyPairFile)) {
+        console.log(`Please place ${giverKeyPairFileName} file in project root folder with Giver's keys`);
+        process.exit(1);
+    }
+
+    const giverKeyPair = JSON.parse(fs.readFileSync(giverKeyPairFile, 'utf8'));
+
+    const params = {
+        send_events: false,
+        message_encode_params: {
+            address: giverAddress,
+            abi: abiContract(GiverV2.abi),
+            call_set: {
+                function_name: 'sendTransaction',
+                input: {
+                    dest: account,
+                    value: 10_000_000_000,
+                    bounce: false
+                }
+            },
+            signer: signerKeys(giverKeyPair)
+        },
+    }
+    await client.processing.process_message(params)
+}
 
 async function main(client) {
 
-    // Let's create message for deploy SafeMultisigWallet contract with 2 owners (custodians), who are authorized to manage the wallet.
+    // Let's create a message to emulate deploy of SafeMultisigWallet contract with 2 owners (custodians), who are authorized to manage the wallet.
+    // Lets generate 2 key pairs for both custodians.
     const multisigKeys = await client.crypto.generate_random_sign_keys();
     const multisigKeys1 = await client.crypto.generate_random_sign_keys();
 
     // Creating signed deploy message.
     // https://github.com/tonlabs/TON-SDK/blob/master/docs/mod_abi.md#encode_message
-    let constructorMessage = await client.abi.encode_message({
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
+    let deploy_message_encode_params = {
+        abi: abiContract(SafeMultisigWalletContract.abi),
         deploy_set: {
             tvc: SafeMultisigWalletContract.tvc,
             initial_data: {}
@@ -37,79 +71,105 @@ async function main(client) {
                 reqConfirms: 2
             }
         },
-        signer: {
-            type: 'Keys',
-            keys: multisigKeys // creates a signed message with provided key pair.
-        }
-    });
+        signer: signerKeys(multisigKeys)
+    };
 
-    // Future account address.
-    const address = constructorMessage.address;
+    let deployMessage = await client.abi.encode_message(deploy_message_encode_params);
+
+    // Calculate future account address.
+    const address = deployMessage.address;
+    console.log("In this sample we are emulating deploy and call of the contract:  ", deployMessage.address);
 
     // Emulate deploy on blockchain using run_executor. In real blockchain we need to sponsor account address for deploy.
     // https://github.com/tonlabs/TON-SDK/blob/master/docs/mod_tvm.md#run_executor
     let result = await client.tvm.run_executor({
-        message: constructorMessage.message,
+        message: deployMessage.message,
         account: {
-            type: 'Uninit', //state of the pre-deployed contract.
+            type: 'Uninit', //state of the pre-paid contract.
         },
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
-        return_updated_account: true // We use return_updated_account to recieve account BOC.
-        // BOC (Bag of cells) is a universal format for data packaging in Free TON. Every object - account, transaction, message, block are stored in blockchain database as bocs. By the way, boc of the block includes bocs of all messages and transactions that were executed in this block inside of it.
+        abi: abiContract(SafeMultisigWalletContract.abi),
+        return_updated_account: true // We use return_updated_account = true to recieve account BOC.
     });
     const deployedAccountBOC = result.account;
     console.log("Emulate contract deploy. Account change state from", result.transaction.orig_status_name, "to", result.transaction.end_status_name);
+    console.log("Estimated deploy fees are: ", result.fees);
 
+    // Now lets deploy and compare estimated and real fees
+    
+    // Request contract deployment funds form a local TON OS SE giver
+    // If you want to run deploy on other networks,
+    // update `get_tokens_from_giver` function so that it uses your contract as a giver.
+    // For instance, you can use Multisig contract  as a giver https://github.com/tonlabs/ton-labs-contracts/tree/master/solidity/safemultisig, 
+    // transfering tokens with `submitTransaction` method. 
+    // Or you can deploy the `GiverV2` contract (https://github.com/tonlabs/tonos-se/tree/master/contracts) 
+    // with your own key pair and update it in GiverV2.keys.json file. 
+
+    console.log("Deploy to the local blockchain.");
+
+    await get_tokens_from_giver(client, address);
+    console.log(`Tokens were transfered from giver to ${address}`);
+
+    const response = await client.processing.process_message({
+        send_events: false,
+        message_encode_params: deploy_message_encode_params
+    });
+    console.log(`Transaction id is ${response.transaction.id}`);
+    console.log(`Deploy fees are  ${JSON.stringify(response.fees, null, 2)}`);
+    console.log(`Contract is successfully deployed. You can play with your multisig wallet now at ${address}`);
+    console.log(`Explore its data at the local version of TON Live http://localhost/accounts/accountDetails?id=0%3A` + `${address.split(':')[1]}`);
+    
     // Let's emulate `submitTransaction` function call.
-    console.log("Emulate submitTransaction function call");
-    submitTransactionMessage = await client.abi.encode_message({
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
+    // Create an external inbound message for emulation of token transfer back to giver
+
+    let submit_encode_message_params = {
+        abi: abiContract(SafeMultisigWalletContract.abi),
         address,
         call_set: {
             function_name: 'submitTransaction',
-            input: {
-                dest: address,
-                value: 100_000_000,
-                bounce: false,
-                allBalance: true,
+            input: { // read about multisig parameters here https://docs.ton.dev/86757ecb2/p/94921e-multisignature-wallet-management-in-tonos-cli/t/4829ad
+                dest: giverAddress,
+                value: 100_000_000, 
+                bounce: false, 
+                allBalance: true, 
                 payload: "",
             }
         },
-        signer: {
-            type: 'Keys',
-            keys: multisigKeys
-        }
-    });
+        signer: signerKeys(multisigKeys)
+    };
+
+    submitTransactionMessage = await client.abi.encode_message(submit_encode_message_params);
 
     // Emulate message proccessing with run_executor.
+    
     result = await client.tvm.run_executor({
         message: submitTransactionMessage.message,
         account: {
             type: 'Account',
             boc: deployedAccountBOC,
-            unlimited_balance: true
+            unlimited_balance: true // this flags emulates unlimited balance on the account
         },
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
-        skip_transaction_check: true,
+        abi: abiContract(SafeMultisigWalletContract.abi),
+       // skip_transaction_check: true, 
         return_updated_account: true
     });
     let newAccountBOC = result.account;
 
+    console.log("Emulate `submitTransaction` function execution.");
+    console.log("Estimated execution fees are: ", result.fees);
+    console.log(`We have just emulated the submition of a transaction with ID = ${result.decoded.output.transId}`);
 
+    // Let's run submitTransaction on-chain and compare fees. 
+    result = await client.processing.process_message({
+        send_events: false,
+        message_encode_params: submit_encode_message_params
+    });
+    console.log(`Transaction id is ${result.transaction.id}`);
+    console.log(`Execution fees are  ${JSON.stringify(result.fees, null, 2)}`);
+
+    // Now lets run a get method over our virtual multisig wallet.
+    // We will retrieve the list of not confirmed transactions. 
     getTransactionsMessage = await client.abi.encode_message({
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
+        abi: abiContract(SafeMultisigWalletContract.abi),
         address,
         call_set: {
             function_name: 'getTransactions',
@@ -118,39 +178,29 @@ async function main(client) {
         signer: { type: 'None' }
     });
 
-    //  Parse BOC to recieved get data from contract.
-    let response = await client.tvm.run_tvm({
+    //  Run multisig's get method `getTransactions` with `run_tvm` function
+    result = await client.tvm.run_tvm({
         message: getTransactionsMessage.message,
         account: newAccountBOC,
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        }
+        abi: abiContract(SafeMultisigWalletContract.abi),
     });
+    let transactions = result.decoded.output.transactions;
+    console.log("Run get method `getTransactions` on the virtual Multisig to get the list of not confirmed transactions."+
+    " We check that the transaction that we have received in the previous step is present. The result is: ", transactions);
 
-    console.log("Contract `getTransactions`: ", response.decoded.output.transactions);
-
-
-    // Let's try another call of contract using run_executor.
-    // Let's confirm previous transaction.
-    let confirmTransactionMessage = await client.abi.encode_message({
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
+    // Let's try to confirm our virtual transaction on our virtual multisig with another custodian using run_executor.
+    let confirm_transacton_params = {
+        abi: abiContract(SafeMultisigWalletContract.abi),
         address,
         call_set: {
             function_name: 'confirmTransaction',
             input: {
-                transactionId: response.decoded.output.transactions[0].id,
+                transactionId: transactions[0].id,
             }
         },
-        signer: {
-            type: 'Keys',
-            keys: multisigKeys1  // Use keys from the second custodian.
-        }
-    });
-    console.log("Sign transaction by second custodian")
+        signer: signerKeys(multisigKeys1) // Use keys from the second custodian.
+    };
+    let confirmTransactionMessage = await client.abi.encode_message(confirm_transacton_params);
 
     result = await client.tvm.run_executor({
         message: confirmTransactionMessage.message,
@@ -159,20 +209,18 @@ async function main(client) {
             boc: newAccountBOC,
             unlimited_balance: true
         },
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
+        abi: abiContract(SafeMultisigWalletContract.abi),
         skip_transaction_check: true,
         return_updated_account: true
     });
     newAccountBOC = result.account;
+    console.log("Emulate `confirmTransaction` function execution.");
+    console.log("Estimated execution fees are: ", result.fees);
+    console.log(`We have just emulated the confirmation of transaction ${transactions[0].id}`);
+    console.log("Let's check that there are no unconfirmed transactions left.");
 
     getTransactionsMessage = await client.abi.encode_message({
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        },
+        abi: abiContract(SafeMultisigWalletContract.abi),
         address,
         call_set: {
             function_name: 'getTransactions',
@@ -181,15 +229,51 @@ async function main(client) {
         signer: { type: 'None' }
     });
 
-    response = await client.tvm.run_tvm({
+    result = await client.tvm.run_tvm({
         message: getTransactionsMessage.message,
         account: newAccountBOC,
-        abi: {
-            type: 'Contract',
-            value: SafeMultisigWalletContract.abi,
-        }
+        abi: abiContract(SafeMultisigWalletContract.abi),
     });
-    console.log("Check that all transactions are signed. `getTransactions` returns: ", response.decoded.output.transactions);
+    transactions = result.decoded.output.transactions
+    console.log("Check that all transactions are signed. `getTransactions` returns: ", transactions);
+    console.log("The list is empty which means we successfully confirmed the virtual transaction.");
+
+
+    // This is basically the essential capabilities of run_execuor function which allows to emulate execution and calculate fees.
+    // If you need to emulate execution with presize account balance then start with top-up from giver emulation. 
+    // To do it you need to create an internal message (with `create_internal_message` function) of transfer from some address (some virtual giver) to your deploy address
+    // and run it on executor, with `unlimited_balance` parameter set to false, Account type = None and skip_transaction_check = true. 
+    // All further calls are the same. 
+    // Like this:
+
+
+    console.log("BONUS. Top-up emulation!");
+    let topup_internal_message_params = {
+        abi: abiContract(SafeMultisigWalletContract.abi),
+            address,
+            src_address: giverAddress,
+            value: "10000000000",
+            bounce: false
+    };
+    let topup_internal_message = await client.abi.encode_internal_message(topup_internal_message_params);
+
+    result = await client.tvm.run_executor({
+        message: topup_internal_message.message,
+        // Non-existing account to run a creation internal message. 
+        // Should be used with skip_transaction_check = true if the message has no deploy data since transactions on the uninitialized account are always aborted
+        account: {
+            type: 'None', 
+        },
+        abi: abiContract(SafeMultisigWalletContract.abi),
+        skip_transaction_check: true,
+        return_updated_account: true // We use return_updated_account = true to recieve account BOC.
+    });
+    console.log("Emulate account top-up. Account's status changed from ", result.transaction.orig_status_name, "to", result.transaction.end_status_name);
+    let account_json = await client.boc.parse_account({boc: result.account});
+    console.log("Account balance is: ", parseInt(account_json.parsed.balance) );
+    console.log("Account state is: ");
+    console.log(account_json);
+
 }
 
 
@@ -197,6 +281,7 @@ async function main(client) {
     const client = new TonClient({
         network: {
             // https://github.com/tonlabs/TON-SDK/blob/master/docs/mod_client.md#clientconfig
+            // Configucation guide: https://docs.ton.dev/86757ecb2/p/5328db-configure-sdk/b/18573c
             endpoints: ["http://localhost"],
         },
     });
