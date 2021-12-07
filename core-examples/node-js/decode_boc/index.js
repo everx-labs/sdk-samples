cd60b08e25b3362ea91273d1e82aa0f8406c44a7
elasticLove1 <elasticLove@protonmail.ch>
2021-12-07 17:29:33 +0300

    samples comments

--- core-examples/node-js/decode_boc/index.js
+++ core-examples/node-js/decode_boc/index.js
@@ -8,6 +8,7 @@ TonClient.useBinaryLibrary(libNode);
         // Decode values from BOC
         const data = (await client.abi.decode_boc({
             boc: "te6ccgEBAgEAEgABCQAAAADAAQAQAAAAAAAAAHs=",
+            // check all abi types here https://github.com/tonlabs/ton-labs-abi/blob/master/docs/ABI_2.1_spec.md#types
             params: [
                 {
                     name: "a",
@@ -15,6 +16,9 @@ TonClient.useBinaryLibrary(libNode);
                 },
                 {
                     name: "b",
+                    // New ABI type `Ref(<ParamType>)` which allows to store `ParamType` ABI parameter 
+                    // in cell reference and, thus, decode manually encoded BOCs.
+                    // Implemented only in decode_boc function, not yet implemented in ABI 
                     type: "ref(int64)",
                 },
                 {
