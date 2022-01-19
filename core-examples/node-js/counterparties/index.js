0a9a562de371f8c63dafb6c1e254e858ad0763dc
Denis K <@tonlabs.io>
2022-01-19 15:05:13 +0500

    Added counterparties example

---
+++ core-examples/node-js/counterparties/index.js
@@ -0,0 +1,58 @@
+const { TonClient } = require("@tonclient/core");
+const { libNode } = require("@tonclient/lib-node");
+
+(async () => {
+    try {
+        // Link the platform-dependable TON-SDK binary with the target Application in Typescript
+        // This is a Node.js project, so we link the application with `libNode` binary 
+        // from `@tonclient/lib-node` package
+        // If you want to use this code on other platforms, such as Web or React-Native,
+        // use  `@tonclient/lib-web` and `@tonclient/lib-react-native` packages accordingly
+        // (see README in  https://github.com/tonlabs/ton-client-js )
+        TonClient.useBinaryLibrary(libNode);
+        client = new TonClient({
+            network: {
+                endpoints: ["main.ton.dev"]
+            }
+        });
+
+        // Get first 5 counterparties:
+        const N = 5;
+        let result = (await client.net.query({
+            query: '{' +
+                `counterparties(account: "-1:3333333333333333333333333333333333333333333333333333333333333333", first: ${N})` +
+                '{account counterparty last_message_at last_message_is_reverse last_message_value(format: DEC)}}'
+        }));
+
+        console.log(`First ${N} counterparties of the Elector address:`)
+        for (const counterparty of result.result.data.counterparties) {
+            console.log(
+                `${counterparty.counterparty}:`,
+                (counterparty.last_message_is_reverse ? '-' : '')
+                + counterparty.last_message_value,
+                'nanotokens',
+                'at',
+                new Date(counterparty.last_message_at * 1000).toLocaleString()
+            );
+        }
+
+        /*
+            Sample output:
+
+            First 5 counterparties of the Elector address:
+            -1:0000000000000000000000000000000000000000000000000000000000000000: -2700735609 nanotokens at 19.01.2022, 15:01:37
+            -1:c9b4d063f26f143bd1eda8fe45935c605d1c7299aee900f0250ba692574819e1: 1000000000 nanotokens at 19.01.2022, 15:01:21
+            -1:90159f0447464201ba7256ddbb5bd8e7cd548e39b643bbd7bef4919de7ec9ad4: 1000000000 nanotokens at 19.01.2022, 15:01:21
+            -1:8ac99aee6b90f5fa2f724cb64cbf7550e304925ec36d5fcc6a45ef1925905c6c: 470865010572206 nanotokens at 19.01.2022, 15:01:21
+            -1:67d82d0ef837213d319ffb437b14bcca2dee1f143559734ebc46b92a75b6d798: 569591491824559 nanotokens at 19.01.2022, 15:01:21
+         */
+
+        process.exit(0);
+    } catch (error) {
+        if (error.code === 504) {
+            console.error(`Network is inaccessible.`);
+        } else {
+            console.error(error);
+        }
+    }
+})();
