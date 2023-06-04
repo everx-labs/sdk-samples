2b0d94baf2c59da35100524139cc1b71ec9f81e2
Goraniya Karan <karangoraniya@gmail.com>
2023-06-04 14:12:01 +0530

    Configure Next.js with transpilation and WebAssembly support 🚀

--- core-examples/next-js/normal-transfer/next.config.js
+++ core-examples/next-js/normal-transfer/next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  transpilePackages: ['@eversdk/lib-web'],
+  webpack(config) {
+    config.output.webassemblyModuleFilename = './public/eversdk.wasm';
+    config.experiments = { asyncWebAssembly: true };
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
